09e900cb94f9b893b3884998b432982798f2989c
DavidChang <chechiachang999@gmail.com>
2017-08-11 18:08:26 +0800

    Finished hamming distance

--- leetcode/easy/hamming_distance.go
+++ leetcode/easy/hamming_distance.go
@@ -1,10 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"strconv"
 )
 
 func HammingDistance(x int, y int) int {
+	if x == y {
+		return 0
+	} else {
+		count := 0
+		for x != 0 || y != 0 {
+			if x%2 != y%2 {
+				count++
+			}
+			x = x >> 1
+			y = y >> 1
+			fmt.Printf("x %v y %v count %v \n", x, y, count)
+		}
+		return count
+	}
+}
+
+func HammingDistance2(x int, y int) int {
 	if x == y {
 		return 0
 	} else {
@@ -16,15 +34,19 @@ func HammingDistance(x int, y int) int {
 				if xs[i] != ys[i] {
 					count++
 				}
+				fmt.Printf("xs[i] %v ys[i] %v count %v \n", xs[i], ys[i], count)
 			}
 			count += len(xs) - len(ys)
+			fmt.Printf("(%v, %v) count %v xs %v ys %v \n", x, y, count, len(xs), len(ys))
 		} else {
 			for i, _ := range xs {
 				if xs[i] != ys[i] {
 					count++
 				}
+				fmt.Printf("xs[i] %v ys[i] %v count %v \n", xs[i], ys[i], count)
 			}
 			count += len(ys) - len(xs)
+			fmt.Printf("(%v, %v) count %v xs %v ys %v \n", x, y, count, len(xs), len(ys))
 		}
 		return count
 	}
--- leetcode/easy/hamming_distance_test.go
+++ leetcode/easy/hamming_distance_test.go
@@ -4,8 +4,14 @@ import (
 	"testing"
 )
 
-func TestHammingDistance(t *testing.T) {
+func TestHammingDistance1(t *testing.T) {
 	if HammingDistance(1, 4) != 2 {
-		t.Error(`TestHammingDistance (1,4) != 2`)
+		t.Error(`Test HammingDistance (1, 4) != 2`)
+	}
+}
+
+func TestHammingDistance2(t *testing.T) {
+	if HammingDistance(4, 14) != 2 {
+		t.Error(`Test HammingDistance (4, 14) != 2`)
 	}
 }
