93ed86ef112a696aa00d4df35f541f6a87c02e2a
David Chang <chechiachang999@gmail.com>
2018-08-30 23:42:21 +0800

    Add twosum hashmap

--- leetcode/easy/two_sum.go
+++ leetcode/easy/two_sum.go
@@ -10,3 +10,19 @@ func twoSumBruteForce(nums []int, target int) []int {
 	}
 	return []int{0, 0}
 }
+
+func twoSumHashmap(nums []int, target int) []int {
+	m := map[int]int{}
+	for i, v := range nums {
+		m[v] = i
+	}
+
+	for i, v := range nums {
+		complement := target - v
+		if _, ok := m[complement]; ok && m[complement] != i {
+			return []int{i, m[complement]}
+		}
+	}
+
+	return []int{0, 0}
+}
--- leetcode/easy/two_sum_test.go
+++ leetcode/easy/two_sum_test.go
@@ -18,3 +18,17 @@ func TestTwoSumBruteForce(t *testing.T) {
 		twoSumBruteForce([]int{3, 2, 4}, 6),
 	)
 }
+
+func TestTwoSumHashmap(t *testing.T) {
+	assert.Equal(
+		t,
+		[]int{0, 1},
+		twoSumHashmap([]int{2, 7, 11, 15}, 9),
+	)
+
+	assert.Equal(
+		t,
+		[]int{1, 2},
+		twoSumHashmap([]int{3, 2, 4}, 6),
+	)
+}
