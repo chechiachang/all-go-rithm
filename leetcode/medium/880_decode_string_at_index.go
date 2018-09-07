c9a91f712a6d8a3ab9b0c55542af727625d38b79
David Chang <chechiachang999@gmail.com>
2018-09-07 10:23:49 +0800

    Add leetcode 880

---
+++ leetcode/medium/880_decode_string_at_index.go
@@ -0,0 +1,19 @@
+package medium
+
+import ()
+
+func DecodeStringAtIndex(S string, K int) {
+	// Brutal approach: print them all!
+
+	// Methamatical approach
+
+	strs := []string{"leet", "code"}
+	//is := []int{2, 3}
+
+	currentIndex := 0
+	for i := 0; i < len(strs); i++ {
+		for currentIndex < K {
+		}
+
+	}
+}
