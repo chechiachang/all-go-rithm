0ddb8db1b232e9c8cbe37d9dd09e5daae7f89c12
David Chang <chechiachang999@gmail.com>
2018-09-13 11:20:35 +0800

    Add order teste

--- datastructures/tree/bst/binary_search_tree_test.go
+++ datastructures/tree/bst/binary_search_tree_test.go
@@ -8,20 +8,26 @@ import (
 func TestSearch(t *testing.T) {
 	bst := BinarySearchTree{}
 
-	bst.Insert(0, "0")
-	r := bst.Search(0)
-	assert.Equal(t, 0, r.key)
-	assert.Equal(t, "0", InorderPrint(bst.root))
-	assert.Equal(t, "0", PreorderPrint(bst.root))
-
 	bst.Insert(1, "1")
-	r = bst.Search(1)
+	r := bst.Search(1)
 	assert.Equal(t, 1, r.key)
-	assert.Equal(t, "01", InorderPrint(bst.root))
-	assert.Equal(t, "01", PreorderPrint(bst.root))
 
 	bst.Insert(2, "2")
+	r = bst.Search(2)
+	assert.Equal(t, 2, r.key)
+}
+
+func TestInorder(t *testing.T) {
+	bst := BinarySearchTree{}
+
+	bst.Insert(4, "4")
+	bst.Insert(5, "5")
+	bst.Insert(6, "6")
+	bst.Insert(7, "7")
 	bst.Insert(3, "3")
-	assert.Equal(t, "0123", InorderPrint(bst.root))
-	assert.Equal(t, "0123", PreorderPrint(bst.root))
+	bst.Insert(2, "2")
+	bst.Insert(1, "1")
+	assert.Equal(t, "1234567", InorderPrint(bst.root))
+	assert.Equal(t, "4321567", PreorderPrint(bst.root))
+	assert.Equal(t, "1237654", PostorderPrint(bst.root))
 }
